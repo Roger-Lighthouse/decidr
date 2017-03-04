83d5c1b642e5c5157d93ea478d5ffa187eb6aeed
Brandon <booth.brandon4@gmail.com>
2017-03-04 19:33:36 +0000

    twilio additions

--- test_helper.js
+++ test_helper.js
@@ -1,5 +1,7 @@
 const helper = require('./routes/helper');
 
 helper.sendText('416-828-8393', 'texting works');
+helper.sendText('647-381-4578', 'texting works');
 
 helper.sendEmail('apple@whiteshark.ca', 'My email', 'My body');
+helper.sendEmail('booth.brandon4@gmail.com', 'My email', 'My body');
