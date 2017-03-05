d70b5b0b0e826cc595f3c69aa2fceff34a462e4a
Roger-Lighthouse <roger@whiteshark.ca>
2017-03-05 14:23:14 +0000

    testing outcall program

--- test_helper.js
+++ test_helper.js
@@ -1,7 +1,13 @@
 const helper = require('./routes/helper');
 
-helper.sendText('416-828-8393', 'texting works');
-helper.sendText('647-381-4578', 'texting works');
+//helper.sendText('416-828-8393', 'texting works');
+//helper.sendText('647-381-4578', 'texting works');
 
-helper.sendEmail('apple@whiteshark.ca', 'My email', 'My body');
-helper.sendEmail('booth.brandon4@gmail.com', 'My email', 'My body');
+//helper.sendEmail('apple@whiteshark.ca', 'My email', 'My body');
+//helper.sendEmail('booth.brandon4@gmail.com', 'My email', 'My body');
+
+//helper.makeCall('4168288393');
+
+helper.makeCall('6476805083');
+
+//helper.makeCall('6475001441');
